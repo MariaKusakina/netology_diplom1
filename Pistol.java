85e935758e2bc8e8e55c0b7cdb59e98a28eea2b4
MariaKusakina <m.a.kusakina@gmail.com>
2022-03-26 17:51:19 +0300

    jhfdhgfdhgfhj

--- Pistol.java
+++ Pistol.java
@@ -4,5 +4,7 @@ public class Pistol extends Weapon {
     @Override
     public void shot() {
         System.out.println("Стреляет пистолет");
+        System.out.println("Стреляет пистолет");
+        System.out.println("Стреляет пистолет");
     }
 }
